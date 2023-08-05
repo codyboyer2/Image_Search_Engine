5980a1c96ceaca1cebf2820bbb9070aa59ec2783
codyboyer2 <codyboyer2@gmail.com>
2023-08-05 17:18:02 -0500

    Uploaded files for search engine

---
+++ script.js
@@ -0,0 +1,55 @@
+// API access key
+const accessKey = config.myKey
+
+// HTML ids linked to js variables
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("result-container");
+const showMoreBtn = document.getElementById("show-more-btn");
+
+let keyword = "";
+let page = 1;
+
+// Search function for API images link
+async function searchImages(){
+    keyword = searchBox.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+
+    // Fetching of API url
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // Remove previous images with new search
+    if(page === 1){
+        searchResult.innerHTML = "";
+    }
+
+    const results = data.results;
+    // Map array of img results and img links
+    results.map((result) =>{
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        const imageLink = document.createElement("a")
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+
+    // Show more button appears after find button clicked
+    showMoreBtn.style.display = "block"
+}
+
+// Prevent default of submit button going to link
+searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    page = 1;
+    searchImages();
+})
+
+// Show more button adds more images to page
+showMoreBtn.addEventListener("click", () => {
+    page++;
+    searchImages();
+})
